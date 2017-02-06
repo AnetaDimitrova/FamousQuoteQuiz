5bddee9da4a44e9e3dcebbe19568ac8c241209eb
AnetaDimitrova <aneta.a.dimitrova@gmail.com>
2017-02-07 00:51:09 +0200

    Added BinaryMode

--- FamousQuoteQuiz.App/Scripts/Custom/applicationJS.js
+++ FamousQuoteQuiz.App/Scripts/Custom/applicationJS.js
@@ -17,7 +17,27 @@
                 $(".hidden_el").show();
             }
         });
+    });
 
+    $(".btn_binMode").click(function () {
+
+
+        var quoteAuthorId = $(".content").attr("id");
+        var authorId = $(".author-name").attr("id");
+        var typeOfAnswer = $(this).attr('id');
+
+        $.ajax({
+            url: "/Main/ChoiseCorrect",
+            method: "POST",
+            data: { quoteAuthorId: quoteAuthorId, authorId: authorId, isAnswerCorrect: typeOfAnswer },
+            success: function (data) {
+                $("#result").html(data);
+                $("#authors_container").hide();
+                $(".btn_binMode").hide();
+                $("#result").show();
+                $(".hidden_el").show();
+            }
+        });
     });
 
     $('.rb_mode').change(function () {
